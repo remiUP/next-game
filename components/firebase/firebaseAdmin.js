82b814b586aa00be5f8b811e4f195f4e34279d3a
Rémi HEITZ <remi.heitz@student-cs.fr>
2021-11-14 18:13:43 +0100

    Firestore admin now working

--- components/firebase/firebaseAdmin.js
+++ components/firebase/firebaseAdmin.js
@@ -1,8 +1,15 @@
-var admin = require("firebase-admin");
+import admin from 'firebase-admin';
+import serviceAccount from '../../firebase.json';
 
-var serviceAccount = require("firebase.json");
+if (!admin.apps.length) {
+	try {
+		admin.initializeApp({
+		credential: admin.credential.cert(serviceAccount),
+		databaseURL: "https://next-game-d05b8-default-rtdb.europe-west1.firebasedatabase.app"
+		});
+	} catch (error) {
+		console.log('Firebase admin initialization error', error.stack);
+	}
+}
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://next-game-d05b8-default-rtdb.europe-west1.firebasedatabase.app"
-});
+export default admin.firestore();
--- pages/api/new.js
+++ pages/api/new.js
@@ -1,13 +1,27 @@
 import jwt from "jsonwebtoken";
+import db  from "../../components/firebase/firebaseAdmin"
+import firebase from "firebase/app"
 
 const KEY = "lqdsfjsdlfkh867dsfjkhg56gsdfhjg567"
+"firebase.database.ServerValue.TIMESTAMP"
+async function createBracket(){
+	const res = await db.collection("brackets").add({
+		test: firebase.firestore.FieldValue.serverTimestamp()
+	})
+	return res.id;
+}
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+	const id = await createBracket();
+	console.log(id)
 	res.status(200).json(
-		{ token: jwt.sign({
-			id : Math.floor(Math.random()*10),
-			name : "test",
-			admin : true
-		},KEY)}
+		{ 
+			token: jwt.sign({
+				id : id,
+				name : "test",
+				admin : true
+			},KEY),
+			id: id
+		}
 	)
   }
