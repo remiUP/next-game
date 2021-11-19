55fbb3faf0c56d4fe1c38783e0011954ca985884
Heitz Remi <remi.heitz@student-cs.fr>
2021-11-19 16:29:08 +0100

    Added pretty hover animations

--- components/bracket/match.js
+++ components/bracket/match.js
@@ -3,7 +3,7 @@ import Team from './team'
 const Match = (props) => {
 	const clickable = props.win == 0 && !props.players.includes('TBD');
 	return (
-	<div className={"font-semibold flex flex-col w-48" + ` ${props.players.length == 1 ? 'hidden md:invisible md:block' : 'visible'}`}>
+	<div className={"font-bold flex flex-col w-48" + ` ${props.players.length == 1 ? 'hidden md:invisible md:block' : 'visible'} mb-3 md:mb-0`}>
 		<Team state={props.win == 1 ? "won" : (props.win == 2 ? "lost" : "")} clickable={clickable} callback={props.upperCallback} player={props.players[0] || "empty"}/>
 		<Team state={props.win == 2 ? "won" : (props.win == 1 ? "lost" : "")} clickable={clickable} callback={props.lowerCallback} player={props.players[1] || "empty"}/>
 	</div>
