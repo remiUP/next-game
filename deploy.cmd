ceeef7d2cf2a9dc9b3bf02bd08e5eda4d3ea659e
unknown <remi.heitz@student-cs.fr>
2022-01-11 12:48:38 +0100

    deploy test 2

--- deploy.cmd
+++
@@ -1,5 +0,0 @@
-ssh pi@192.168.1.102
-cd next-game
-git pull
-npm run build
-pm2 reload next-game
---
+++ deploy.py
@@ -0,0 +1,28 @@
+from paramiko import SSHClient, AutoAddPolicy
+
+
+commands = [
+		'cd next-game',
+		'git pull',
+		'npm run build',
+		'pm2 reload next-game'
+	]
+
+
+client = SSHClient()
+client.set_missing_host_key_policy(AutoAddPolicy())
+client.connect('192.168.1.102', username='pi')
+command = ' && '.join(commands)
+stdin, stdout, stderr = client.exec_command(command)
+print(command)
+
+stdin.channel.shutdown_write()
+
+print(f'STDOUT: {stdout.read().decode("utf8")}')
+print(f'STDERR: {stderr.read().decode("utf8")}')
+
+stdin.close()
+stdout.close()
+stderr.close()
+
+client.close()
