c6d1694ee5cd5bd3d75660302115eed3eaad30ee
remiUP <rdsdf@hotmail.fr>
2022-02-27 15:48:45 +0100

    Fixed margin bottom

--- components/bracket/match.tsx
+++ components/bracket/match.tsx
@@ -35,7 +35,7 @@ const Match: React.FC<Props> = (props) => {
 	}
 	
 	return (
-		<div className={"font-bold flex flex-col w-48 border-l-4 border-blue-700 " + ` ${props.players.length == 1 ? 'hidden md:invisible md:block' : 'visible'} mb-3 md:mb-4`}>
+		<div className={"font-bold flex flex-col w-48 border-l-4 border-blue-700 " + ` ${props.players.length == 1 ? 'hidden md:invisible md:block' : 'visible'} mb-3 md:mb-0`}>
 			<Team state={props.win == 1 ? "won" : (props.win == 2 ? "lost" : "")} clickable={clickable} callback={() => registerResult(0)} player={props.players[0] || "⠀"}/>
 			<Team state={props.win == 2 ? "won" : (props.win == 1 ? "lost" : "")} clickable={clickable} callback={() => registerResult(1)} player={props.players[1] || "⠀"} bottom={true}/>
 		</div>
